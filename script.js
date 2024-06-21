62a2c0edc1759c5040f20b4ac22ee181122698ed
Faiqa <faiqaabbas474@gmail.com>
2024-06-21 10:01:44 +0500

    Applied Lenis with fixed position

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+// locomotive
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('.main'),
+//     smooth: true
+// });
+
+// Lenis
+const lenis = new Lenis();
+lenis.on("scroll", (e) => {
+  // console.log(e);
 });
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
